ef226d90e1509a6b7b46607536465a30c9fe9b56
Dawid <dawid.zygmunt86@gmail.com>
2024-10-23 14:38:38 +0200

    report bug form Main menu

--- app/(routes)/report-bug/page.tsx
+++ app/(routes)/report-bug/page.tsx
@@ -7,19 +7,25 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
 import { zodResolver } from "@hookform/resolvers/zod"
+import Image from "next/image"
 import { useForm } from "react-hook-form"
 import toast from "react-hot-toast"
 import { z } from "zod"
 
 const FormSchema = z.object({
   content: z.string().min(5),
-  priority: z.coerce.number().int().min(1).max(5),
+  priority: z.string(),
 })
 
 const ReportBug = () => {
@@ -27,14 +33,17 @@ const ReportBug = () => {
     resolver: zodResolver(FormSchema),
     defaultValues: {
       content: "",
-      priority: 1,
     },
   })
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
     try {
-      const ticket = await CreateTicket(data)
-      toast.success("Activity added successfully")
+      const parsedData = {
+        content: data.content,
+        priority: Number(data.priority),
+      }
+      const ticket = await CreateTicket(parsedData)
+      toast.success("Send successfully")
     } catch (error) {
       toast.error("An error occurred")
       return
@@ -42,50 +51,70 @@ const ReportBug = () => {
   }
 
   return (
-    <div className="flex w-full pt-20 justify-center">
-      <div className="">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="content"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Describe your problem</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Describe your problem"
-                      className="resize-y min-h-[150px]"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+    <div className="flex w-full min-h-screen justify-center items-center bg-gradient-to-r from-violet-300 to-fuchsia-300">
+      <div className="w-full lg:w-auto grid grid-cols-1 md:grid-cols-2 lg:px-24 ">
+        <div className="rounded-s-xl shadow-xl md:max-h-[80vh]">
+          <Image
+            src="/cards/bug-image.jpg"
+            alt="Descriptive text for screen readers "
+            width={600}
+            height={700}
+            className="rounded-s-xl object-cover md:max-h-[80vh] w-full h-full "
+          />
+        </div>
 
-            <FormField
-              control={form.control}
-              name="priority"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>How important it is? | 1 - 5</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Put your priority"
-                      type="number"
-                      min={1}
-                      max={5}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
+        <div className="flex flex-col items-start px-12 lg:w-full lg:h-full bg-[#ffffff] shadow-xl text-[#203020] p-10 rounded-e-xl">
+          <h2 className="text-2xl text-left mb-7">Zgłoś błąd</h2>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-6 w-full "
+            >
+              <FormField
+                control={form.control}
+                name="priority"
+                render={({ field }) => (
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Wybierz wagę problemu" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="1">niska</SelectItem>
+                      <SelectItem value="2">średnia</SelectItem>
+                      <SelectItem value="3">wysoka</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormControl>
+                      <Textarea
+                        placeholder="Opowiedz nam o tym, co się stało"
+                        className="resize-y min-h-[250px] max-h-[350px]"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="rounded-3xl px-10 bg-purple-700">
+                Wyślij
+              </Button>
+            </form>
+          </Form>
+        </div>
       </div>
     </div>
   )
